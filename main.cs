7aab83a61471765fd0243b9545b4b6a893da5713
ranma15g <ranma15g@gmail.com>
2024-05-13 14:41:26 +0000

    I'm messing with my code

--- main.cs
+++ main.cs
@@ -2,6 +2,6 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World, my name is Eric Snyder! Pleased to meet you!");
+    Console.WriteLine ("Hello World, my name is Eric Snyder! Pleased to meet you twice now!");
   }
 }
